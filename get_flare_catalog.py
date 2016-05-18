7a47c7cd76a33bce674dfba32a4cbced64033c1e
Alysha Reinard <alysha.reinard@noaa.gov>
2016-05-18 14:19:40 -0600

    small changes

--- get_flare_catalog.py
+++ get_flare_catalog.py
@@ -20,13 +20,159 @@ def check_daymonth(day, month, year):
 
                 
 def get_flare_catalog():
-    """ program to read in GOES h-alpha and x-ray flare information"""
+    """ program to read in GOES h-alpha and x-ray flare information from web"""
     """ usage: [ha, xray]=get_flare_catalog; ha is a dict"""
     """ ha['location'][300] prints the 300th location"""
     """ keys are ha.keys() -- station_num, group_num, initial_time, final_time"""
     """ peak_time, optical_importance, optical_brightness, xray_class, """
     """ xray_size, NOAA_AR """
 
+    web_stem="http://www.ngdc.noaa.gov/stp/space-weather/solar-data/solar-features/solar-flares/x-rays/goes/xrs/"
+    #Fill in X-ray values
+    #prepare lists for variables
+    group_num=[]
+    station_num=[]
+    initial_time=[]
+    final_time=[]
+    peak_time=[]
+    initial_indicator=[]
+    final_indicator=[]
+    peak_indicator=[]
+    location=[]
+    optical_importance=[]
+    optical_brightness=[]
+    xray_class=[]
+    xray_size=[]
+    NOAA_AR=[]
+
+
+    for getyear in range(1981, 2016):
+        print(getyear)
+        webpage=web_stem+"goes-xrs-report_"+str(getyear)+".txt"
+        line="nothing read yet"
+#        try: 
+        data=urlopen(webpage)
+#        data=data.decode('utf-8')
+#        print(data)
+    #        data=data.split("\n")
+        init_missing=0
+        final_missing=0
+        peak_missing=0
+        for line in data:
+            line=line.decode('utf-8')  
+
+            group_num.append(line[0:2])
+            station_num.append(line[2:4])
+            try:
+                year=int(line[5:7])
+            except:
+                break
+            if year < 70:
+                year=year+2000
+            else:
+                year=year+1900
+            peak_year=initial_year=final_year=year
+            peak_month=initial_month=final_month=month=int(line[7:9])
+            peak_day=initial_day=final_day=day=int(line[9:11])
+    
+            initial_indicator.append(line[17])
+            final_indicator.append(line[22])
+            peak_indicator.append(line[27])
+            location.append(line[28:34])
+            optical_importance.append(line[34:35])
+            optical_brightness.append(line[35:36])
+            xray_class.append(line[59:60])
+            xray_size.append(line[61:63])
+            NOAA_AR.append(line[81:85])
+            
+            try:
+                initial_hr=int(line[13:15])
+                initial_min=int(line[15:17])
+            except ValueError:
+                init_missing+=1
+    #            print("No initial time for event on date year:%d, month: %d, day: %d" %(year, month, day))
+    
+            try:
+                final_hr=int(line[18:20])
+                final_min=int(line[20:22])
+            except ValueError:
+                final_missing+=1
+    #            print("No final time for event on date year:%d, month: %d, day: %d" %(year, month, day))
+    
+            try:
+                peak_hr=int(line[23:25])
+                peak_min=int(line[25:27])
+            except ValueError:
+                peak_missing+=1
+    #            print("No peak time for event on date year:%d, month: %d, day: %d" %(year, month, day))
+    
+            try:
+                if initial_hr==99:
+                    initial_time.append(None)
+                else:
+                    if initial_hr>=24:
+                        initial_hr=initial_hr-24
+                        initial_day=initial_day+1
+                    [initial_day, initial_month, initial_year]=check_daymonth(initial_day, initial_month, peak_year)
+                    initial_time.append(datetime(initial_year, initial_month, initial_day, initial_hr, initial_min))
+            except ValueError:
+                print(line)
+                print("Initial date is not valid: year: %d, month:%d, day:%d, hour:%d, min:%d" % (year, month, initial_day, initial_hr, initial_min))
+                initial_time.append(None)
+            except NameError:
+                initial_time.append(None)
+    
+            try:
+                if final_hr==99:
+                    final_time.append(None)
+                else:
+                    final_day=day
+                    if final_hr>=24:
+                        final_hr=final_hr-24
+                        final_day=final_day+1
+                    [final_day, final_month, final_year]=check_daymonth(final_day, final_month, final_year)
+                    final_time.append(datetime(final_year, final_month, final_day, final_hr, final_min))
+            except ValueError:
+                print(line)
+                print("Final date is not valid: year: %d, month:%d, day:%d, hour:%d, min:%d" % (year, month, final_day, final_hr, final_min))
+                final_time.append(None)
+            except NameError:
+                final_time.append(None)
+    
+            try:
+                if peak_hr==99:
+                    peak_time.append(None)
+                else:
+                    peak_day=day
+                    if peak_hr>=24:
+                        peak_hr=peak_hr-24
+                        peak_day=peak_day+1
+                    [peak_day, peak_month, peak_year]=check_daymonth(peak_day, peak_month, peak_year)
+                    peak_time.append(datetime(peak_year, peak_month, peak_day, peak_hr, peak_min))
+            except ValueError:
+                print(line)
+                print("Peak date is not valid: year: %d, month:%d, day:%d, hour:%d, min:%d" % (year, month, peak_day, peak_hr, peak_min))
+                peak_time.append(None)
+            except NameError:
+                peak_time.append(None)
+        
+    xray_flares={'group_num':group_num, 'station_num':station_num,
+               'initial_time':initial_time, 'final_time':final_time,
+               'peak_time':peak_time, 'location':location,
+               'optical_importance':optical_importance, 'optical_brightness':optical_brightness,
+               'xray_class':xray_class, 'xray_size':xray_size, "NOAA_AR":NOAA_AR}
+
+    ha_flares={"not yet implemented":"try get_flare_catalog_fromfile()"}
+    return [ha_flares, xray_flares]    
+
+        
+def get_flare_catalog_fromfile():
+    """ program to read in GOES h-alpha and x-ray flare information from file"""
+    """ usage: [ha, xray]=get_flare_catalog; ha is a dict"""
+    """ ha['location'][300] prints the 300th location"""
+    """ keys are ha.keys() -- station_num, group_num, initial_time, final_time"""
+    """ peak_time, optical_importance, optical_brightness, xray_class, """
+    """ xray_size, NOAA_AR """
     #define data file location
     data_dir=os.getcwd()+"/data"
     data_dir="/Users/alyshareinard/Documents/python/common/data"
@@ -188,7 +334,7 @@ def get_flare_catalog():
         group_num.append(line[0:2])
         station_num.append(line[2:4])
         year=int(line[5:7])
-        if year < 90:
+        if year < 70:
             year=year+2000
         else:
             year=year+1900
