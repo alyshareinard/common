55192693f6b6967a9aeb2dc190ae1547e69b079f
Alysha Reinard <1393569723@mil>
2017-01-18 17:06:49 -0700

    cleaned up outputs to file

--- get_flare_catalog.py
+++ get_flare_catalog.py
@@ -65,14 +65,14 @@ def check_daymonth(day, month, year):
     return [day, month, year]
         
                 
-def download_flare_catalog():
+def download_flare_catalog(start_year, stop_year):
     """ program to read in GOES h-alpha and x-ray flare information from web"""
     """ usage: [ha, xray]=get_flare_catalog; ha is a pandas dataframe"""
     """ ha['location'][300] prints the 300th location"""
 
     count=0
     #strange character at the end of teh 2003 file causes trouble
-    for getyear in range(2014, 2016):  
+    for getyear in range(start_year, stop_year):  
         print("year: ", getyear)
         web_stem="http://www.ngdc.noaa.gov/stp/space-weather/solar-data/solar-features/solar-flares/x-rays/goes/xrs/"
         xray_webpage=web_stem+"goes-xrs-report_"+str(getyear)+".txt"
@@ -97,9 +97,10 @@ def download_flare_catalog():
         xray_df_year["init_date"]=create_datetime(xray_df_year["year_month_day"], xray_df_year["init_time"])
         xray_df_year["peak_date"]=create_datetime(xray_df_year["year_month_day"], xray_df_year["peak_time"])
         xray_df_year["final_date"]=create_datetime(xray_df_year["year_month_day"], xray_df_year["final_time"])
-    
+        xray_df_year["location"]=[x if type(x) is str else "      " for x in xray_df_year["location"]]
+
         xray_df_year=xray_df_year[["init_date", "peak_date", "final_date", "location", "xray_class", "xray_size", "NOAA_AR"]]
-           
+        
           
 #        names=["data code", "station code", "year", "month", "day", "init_ind", "init_time", "final_ind", "final_time", "peak_ind", 
 #               "peak_time", "location", "data source", "something", "xray_class", "xray_size", "station", "optical", "int_flux", 
@@ -129,161 +130,7 @@ def download_flare_catalog():
             xray_df=pd.concat(dfs)
     ha_df="not yet implemented"
     return (xray_df, ha_df)
-    #xray_pd=pd.DataFrame(data, widths=widths, header=None, columns=names, parse_dates=[[2, 3, 4]])
 
-
-#    for line in data:
-#        print(line.decode('utf-8'))
-    #print(xray_pd.head())
-
-
-
-    #Fill in X-ray values
-    #prepare lists for variables
-    group_num=[]
-    station_num=[]
-    initial_time=[]
-    final_time=[]
-    peak_time=[]
-    initial_indicator=[]
-    final_indicator=[]
-    peak_indicator=[]
-    location=[]
-    optical_importance=[]
-    optical_brightness=[]
-    xray_class=[]
-    xray_size=[]
-    NOAA_AR=[]
-
-
-#    for getyear in range(1981, 2016):
-    for getyear in range(2013, 2016):
- 
-        print(getyear)
-        webpage=web_stem+"goes-xrs-report_"+str(getyear)+".txt"
-        print(webpage)
-        line="nothing read yet"
-#        try: 
-        data=urlopen(webpage)
-#        data=data.decode('utf-8')
-#        print(data)
-    #        data=data.split("\n")
-        init_missing=0
-        final_missing=0
-        peak_missing=0
-        for line in data:
-            line=line.decode('utf-8')  
-
-            group_num.append(line[0:2])
-            station_num.append(line[2:4])
-            try:
-                year=int(line[5:7])
-            except:
-                break
-            if year < 70:
-                year=year+2000
-            else:
-                year=year+1900
-            peak_year=initial_year=final_year=year
-            peak_month=initial_month=final_month=month=int(line[7:9])
-            peak_day=initial_day=final_day=day=int(line[9:11])
-    
-            peak_year=create_datetime(ha_df["year_month_day"], line[13:17])
-            initial_indicator.append(line[17])
-            final_indicator.append(line[22])
-            peak_indicator.append(line[27])
-            location.append(line[28:34])
-            optical_importance.append(line[34:35])
-            optical_brightness.append(line[35:36])
-            xray_class.append(line[59:60])
-            xray_size.append(line[61:63])
-            NOAA_AR.append(line[81:85])
-            
-            try:
-                initial_hr=int(line[13:15])
-                initial_min=int(line[15:17])
-            except ValueError:
-                init_missing+=1
-    #            print("No initial time for event on date year:%d, month: %d, day: %d" %(year, month, day))
-    
-            try:
-                final_hr=int(line[18:20])
-                final_min=int(line[20:22])
-            except ValueError:
-                final_missing+=1
-    #            print("No final time for event on date year:%d, month: %d, day: %d" %(year, month, day))
-    
-            try:
-                peak_hr=int(line[23:25])
-                peak_min=int(line[25:27])
-            except ValueError:
-                peak_missing+=1
-    #            print("No peak time for event on date year:%d, month: %d, day: %d" %(year, month, day))
-    
-            try:
-                if initial_hr==99:
-                    initial_time.append(None)
-                else:
-                    if initial_hr>=24:
-                        initial_hr=initial_hr-24
-                        initial_day=initial_day+1
-                    [initial_day, initial_month, initial_year]=check_daymonth(initial_day, initial_month, peak_year)
-                    initial_time.append(datetime(initial_year, initial_month, initial_day, initial_hr, initial_min))
-            except ValueError:
-                print(line)
-                print("Initial date is not valid: year: %d, month:%d, day:%d, hour:%d, min:%d" % (year, month, initial_day, initial_hr, initial_min))
-                initial_time.append(None)
-            except NameError:
-                initial_time.append(None)
-    
-            try:
-                if final_hr==99:
-                    final_time.append(None)
-                else:
-                    final_day=day
-                    if final_hr>=24:
-                        final_hr=final_hr-24
-                        final_day=final_day+1
-                    [final_day, final_month, final_year]=check_daymonth(final_day, final_month, final_year)
-                    final_time.append(datetime(final_year, final_month, final_day, final_hr, final_min))
-            except ValueError:
-                print(line)
-                print("Final date is not valid: year: %d, month:%d, day:%d, hour:%d, min:%d" % (year, month, final_day, final_hr, final_min))
-                final_time.append(None)
-            except NameError:
-                final_time.append(None)
-    
-            try:
-                if peak_hr==99:
-                    peak_time.append(None)
-                else:
-                    peak_day=day
-                    if peak_hr>=24:
-                        peak_hr=peak_hr-24
-                        peak_day=peak_day+1
-                    [peak_day, peak_month, peak_year]=check_daymonth(peak_day, peak_month, peak_year)
-                    peak_time.append(datetime(peak_year, peak_month, peak_day, peak_hr, peak_min))
-            except ValueError:
-                print(line)
-                print("Peak date is not valid: year: %d, month:%d, day:%d, hour:%d, min:%d" % (year, month, peak_day, peak_hr, peak_min))
-                peak_time.append(None)
-            except NameError:
-                peak_time.append(None)
-        
-    xray_flares={'group_num':group_num, 'station_num':station_num,
-               'initial_time':initial_time, 'final_time':final_time,
-               'peak_time':peak_time, 'location':location,
-               'optical_importance':optical_importance, 'optical_brightness':optical_brightness,
-               'xray_class':xray_class, 'xray_size':xray_size, "NOAA_AR":NOAA_AR}
-
-#    filehandler=open('data/haflare_vals.p', 'wb')
-#    pickle.dump(ha_flares, filehandler)
-    data_dir="/Users/alyshareinard/Documents/python/common/data"
-
-    filehandler=open(data_dir+'/xflare_vals.p', 'wb')
-    pickle.dump(xray_flares, filehandler)
-    ha_flares={"not yet implemented":"try get_flare_catalog_fromfile()"}
-    return (ha_flares, xray_flares)
 
         
 def get_flare_catalog_fromfile():
@@ -332,13 +179,13 @@ def get_flare_catalog_fromfile():
 
     return (xray_df, ha_df)
 
-def get_flare_catalog():
+def get_flare_catalog(start_year=2013, stop_year=2014):
 
     try:
         print("downloading")
-        (xray, halpha)=download_flare_catalog()
+        (xray, halpha)=download_flare_catalog(start_year, stop_year)
     except:
-        print("getting from file")
+        print("getting from file, years are only those downloaded")
         (xray, halpha)=get_flare_catalog_fromfile()
     return (xray, halpha)
                             
